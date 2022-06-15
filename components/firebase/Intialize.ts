3ba2c8881444063238dabe68373f110167f3950c
Justin Morris <jnatmorris@gmail.com>
2022-06-15 18:24:10 -0400

    update env

--- components/firebase/Intialize.ts
+++ components/firebase/Intialize.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
+    apiKey: "AIzaSyCdiNx1IBYXdG7_IeloWZfgiwGCpRTfXHU",
+    authDomain: "uni-market-f02ac.firebaseapp.com",
+    databaseURL: "https://uni-market-f02ac-default-rtdb.firebaseio.com",
+    projectId: "uni-market-f02ac",
+    storageBucket: "uni-market-f02ac.appspot.com",
+    messagingSenderId: "907050157185",
+    appId: "1:907050157185:web:3049a17c08ce7cadccf980",
 };
 
 export const app = initializeApp(firebaseConfig);
