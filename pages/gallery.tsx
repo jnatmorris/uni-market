2f2bd927fa198ee89756605f2f0a7a5593d4cda6
Justin Morris <jnatmorris@gmail.com>
2022-06-16 11:38:48 -0400

    add ssr

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,25 +1,79 @@
 import React from "react";
-import { ReadData } from "../components/firebase/db/actions";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../components/firebase/Intialize";
+import { GetServerSideProps } from "next";
 
-// export async function getServerSideProps() {
-//     const posts = [""]
-//     return {
-//         props: { posts }, // will be passed to the page component as props
-//     };
-// }
+export const getServerSideProps: GetServerSideProps = async () => {
+    const items = new Array();
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+    });
 
-// const Gallery:React.FC = ({posts}) => {
+    return {
+        props: { obj: { items } },
+    };
+};
 
-//     return <div>
+interface Props {
+    obj: {
+        items: {
+            username: string;
+            author: string;
+            itemDesc: string;
+            itemName: string;
+            price: number;
+        }[];
+    };
+}
 
-//     </div>
-// }
+const Gallery: React.FC<Props> = ({ obj }) => {
+    console.log(obj.items);
+    return (
+        <div>
+            <h1 className="py-[5vh]">Uni Marketplace</h1>
+            <div className="grid grid-cols-3 gap-2">
+                {obj.items.map((value, index) => {
+                    const { username, author, itemDesc, itemName, price } =
+                        value;
+                    return (
+                        <div
+                            key={index}
+                            className="p-2 rounded-2xl bg-slate-200"
+                        >
+                            {/* temp image */}
+                            <div className="h-48 bg-white rounded-lg"></div>
 
-const Gallery = () => {
-    React.useEffect(() => {
-        ReadData();
-    }, []);
-    return <div></div>;
+                            <h3 className="">{itemName}</h3>
+
+                            <div className="h-[0.15rem] my-1 bg-black" />
+
+                            <div className="space-y-1">
+                                <p>
+                                    <span className="font-semibold">
+                                        Sold by:
+                                    </span>{" "}
+                                    {username}
+                                </p>
+                                <p>
+                                    <span className="font-semibold">
+                                        Price:
+                                    </span>{" "}
+                                    {price}
+                                </p>
+                                <p>
+                                    <span className="font-semibold">
+                                        Item Discription:
+                                    </span>{" "}
+                                    {itemDesc}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Gallery;
