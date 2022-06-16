346b9a98664e46d2944e2dbf82d579255fb2731d
Justin Morris <jnatmorris@gmail.com>
2022-06-16 11:39:32 -0400

    update styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }) => {
     return (
-        <main className="prose dark:prose-invert ">
+        <main className="prose prose-h1:m-0 prose-h2:m-0 prose-h3:m-0 prose-h4:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
             <div className="w-screen">
                 <div className="mx-[5vw]">
                     <Navigation />
-                    {children}
+                    <div className="">{children}</div>
                     <Footer />
                 </div>
             </div>
