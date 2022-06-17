659827825f32d3a53467c86c7f7b4bc3d156a1b7
Justin Morris <jnatmorris@gmail.com>
2022-06-17 14:37:29 -0400

    add comments

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // images from firbase
     images: {
         domains: ["firebasestorage.googleapis.com"],
     },
-    reactStrictMode: true,
-    swcMinify: true,
 };
 
 module.exports = nextConfig;
