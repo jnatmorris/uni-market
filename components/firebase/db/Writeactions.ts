5b6e1d16e2130c27cf55e9ab5b5283a5c3c4bbf9
Justin Morris <jnatmorris@gmail.com>
2022-06-17 12:57:22 -0400

    move actions

--- components/firebase/db/Writeactions.ts
+++ components/firebase/db/Writeactions.ts
@@ -1,37 +1,5 @@
 import { doc, setDoc, collection } from "firebase/firestore";
 import { db } from "../Intialize";
-import { GetUsername } from "./ReadActions";
-
-const AddPost = (
-    itemName: string,
-    itemDesc: string,
-    userID: string,
-    price: number,
-    clearInputHandler: () => void
-) => {
-    try {
-        GetUsername(userID).then((username) => {
-            // generate random ID
-            const ref = doc(collection(db, "posts"));
-
-            console.log(username);
-
-            // Add a new document in collection of posts with following data
-            setDoc(ref, {
-                // username: username,
-                username: username,
-                author: userID,
-                itemName: itemName,
-                itemDesc: itemDesc,
-                price: price,
-            });
-            // revert UI to allow another post
-            clearInputHandler();
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
 
 const SignUpUsername = async (
     userID: string,
@@ -43,4 +11,4 @@ const SignUpUsername = async (
     });
 };
 
-export { AddPost, SignUpUsername };
+export { SignUpUsername };
