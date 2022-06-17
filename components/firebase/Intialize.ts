e320d12e8303b3085537a84df4758fefc2943a36
Justin Morris <jnatmorris@gmail.com>
2022-06-17 12:56:51 -0400

    add firebase storage

--- components/firebase/Intialize.ts
+++ components/firebase/Intialize.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCdiNx1IBYXdG7_IeloWZfgiwGCpRTfXHU",
@@ -15,3 +16,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+export const storage = getStorage(app);
