615598890e3e071f32a51a061a213dc470b8acad
Justin Morris <jnatmorris@gmail.com>
2022-06-17 12:56:20 -0400

    updated domain for image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ["firebasestorage.googleapis.com"],
+    },
     reactStrictMode: true,
+
     swcMinify: true,
 };
 
