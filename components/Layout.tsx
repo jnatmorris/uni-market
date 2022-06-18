32bcfe31aa1c5f79a9103afb8926c5cbf102c3a5
Justin Morris <jnatmorris@gmail.com>
2022-06-18 09:58:44 -0400

    clean layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,14 +8,10 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }) => {
     return (
-        <main className="prose prose-h1:m-0 prose-h2:m-0 prose-h3:m-0 prose-h4:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
-            <div className="w-screen">
-                <div className="mx-[3vw]">
-                    <Navigation />
-                    {children}
-                    <Footer />
-                </div>
-            </div>
+        <main>
+            <Navigation />
+            {children}
+            <Footer />
         </main>
     );
 };
