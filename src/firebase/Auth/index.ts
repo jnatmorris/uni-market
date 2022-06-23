26482c2444e640737acb0cd9715bee33258154aa
Justin Morris <jnatmorris@gmail.com>
2022-06-23 13:19:56 -0400

    all export from single file

---
+++ src/firebase/Auth/index.ts
@@ -0,0 +1,19 @@
+import { LoginUser } from "./utils/LoginUser";
+import { GetCurrentInfo } from "./utils/GetCurrentInfo";
+import { SingOutUser } from "./utils/SignoutUser";
+import { SetNewPass } from "./utils/SetNewPass";
+import { ResetPassword } from "./utils/ResetPassword";
+import { AddUser } from "./utils/AddUser";
+import {AuthProvider, AuthContext} from "./api/AuthProvider"
+
+// import { AddUser } from "./utils/AddUser";
+
+export {
+    LoginUser,
+    GetCurrentInfo,
+    SingOutUser,
+    SetNewPass,
+    ResetPassword,
+    AddUser,
+    AuthProvider, AuthContext
+};
---
+++ src/firebase/db/index.ts
@@ -0,0 +1,17 @@
+import { AddNewPost } from "./utils/AddNewPost";
+import { GetAllPosts } from "./utils/GetAllPosts";
+import { GetUsername } from "./utils/GetUsername";
+import { GetUsernamePosts } from "./utils/GetUsernamePosts";
+import { SignUpUsername } from "./utils/SignupUsername";
+import { StorePostImage } from "./utils/StorePostImage";
+import { ValidateUsername } from "./utils/ValidateUsername";
+
+export {
+    AddNewPost,
+    GetAllPosts,
+    GetUsername,
+    GetUsernamePosts,
+    SignUpUsername,
+    StorePostImage,
+    ValidateUsername,
+};
