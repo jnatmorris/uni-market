f88b6e646a2795e61687078ccc884c315f913873
Justin Morris <jnatmorris@gmail.com>
2022-06-28 09:28:49 -0400

    updated path

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { NextPage } from "next";
 import { GetServerSideProps } from "next";
-import DisplayItem from "../../src/Components/DisplayItem";
+import { DisplayItem } from "../../src/Components/DisplayItem";
 import { ValidateUsername, GetUsernamePosts } from "@db/index";
 
 import MetaTags from "../../src/Components/Metatags";
